df12b47403c41281ee7e24297ef3f77cea4f561b
lfal703 <146048320+lfal703@users.noreply.github.com>
2024-04-05 16:11:03 -0400

    Removed placeholder sites

--- js/sites.js
+++ js/sites.js
@@ -3,56 +3,6 @@ const sites = [
         "name" : "Leonardo Falvo",
         "year" : "2026", //inshallah
         "website" : "https://leofalvo.github.io"
-    },
-    {
-        "name" : "Google guy",
-        "year" : "123", //inshallah
-        "website" : "https://google.com"
-    },
-    {
-        "name" : "Google guy",
-        "year" : "123", //inshallah
-        "website" : "https://google.com"
-    },
-    {
-        "name" : "Google guy",
-        "year" : "123", //inshallah
-        "website" : "https://google.com"
-    },
-    {
-        "name" : "Google guy",
-        "year" : "123", //inshallah
-        "website" : "https://google.com"
-    },
-    {
-        "name" : "Google guy",
-        "year" : "123", //inshallah
-        "website" : "https://google.com"
-    },
-    {
-        "name" : "Google guy",
-        "year" : "123", //inshallah
-        "website" : "https://google.com"
-    },
-    {
-        "name" : "Google guy",
-        "year" : "123", //inshallah
-        "website" : "https://google.com"
-    },
-    {
-        "name" : "Google guy",
-        "year" : "123", //inshallah
-        "website" : "https://google.com"
-    },
-    {
-        "name" : "Google guy",
-        "year" : "123", //inshallah
-        "website" : "https://google.com"
-    },
-    {
-        "name" : "Google guy",
-        "year" : "123", //inshallah
-        "website" : "https://google.com"
     }
 ]
 export default sites;
