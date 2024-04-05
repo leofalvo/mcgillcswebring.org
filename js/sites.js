309c2799e62ee05a49e5f809edd9342df1fc34a8
aditipotnis <165708174+aditipotnis@users.noreply.github.com>
2024-04-05 17:10:59 -0400

    added name sites.js

--- js/sites.js
+++ js/sites.js
@@ -3,6 +3,11 @@ const sites = [
         "name" : "Leonardo Falvo",
         "year" : "2026", //inshallah
         "website" : "https://leofalvo.github.io"
+    },
+    {
+        "name" : "Aditi Potnis",
+        "year" : "2026",
+        "website" : "xx"
     }
 ]
 export default sites;
