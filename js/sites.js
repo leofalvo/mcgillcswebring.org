ac94ccdf251d9643b939550c0b6e708db9cb12c6
Leo <146048320+leofalvo@users.noreply.github.com>
2024-04-14 21:09:50 -0400

    Update sites.js

--- js/sites.js
+++ js/sites.js
@@ -1,29 +1,28 @@
 const sites = [
     {
-        "name" : "Leo Falvo",
-        "year" : "2026",
-        "website" : "https://leofalvo.com"
+        "name": "Leo Falvo",
+        "year": "2026",
+        "website": "https://leofalvo.com"
     },
     {
-        "name" : "Aditi Potnis",
-        "year" : "2026",
-        "website" : "https://github.com/aditipotnis"
+        "name": "Aditi Potnis",
+        "year": "2026",
+        "website": "https://github.com/aditipotnis"
     },
     {
-        "name" : "Swagat Bhowmik",
-        "year" : "2026",
-        "website" : "https://github.com/Swagat404"
+        "name": "Swagat Bhowmik",
+        "year": "2026",
+        "website": "https://github.com/Swagat404"
     },
     {
-        "name" : "Reshu <3",
-        "year" : "2026",
-        "website" : "https://reswanthpillai.wixsite.com/website"
+        "name": "Reshu <3",
+        "year": "2026",
+        "website": "https://reswanthpillai.wixsite.com/website"
     },
     {
-        "name" : "Zadig",
-        "year" : 2026",
-        "website" : "https://github.com/Z333333333333"
+        "name": "Zadig",
+        "year": "2026",
+        "website": "https://github.com/Z333333333333"
     }
-
-]
+];
 export default sites;
